36e12bdd12663392837b2faf643e45a36b1f6eee
itsleaht <leaztanda@gmail.com>
2025-06-18 18:33:03 +0200

    image: wip image formatter form

--- src/features/images/components/ImageFormatter/index.tsx
+++ src/features/images/components/ImageFormatter/index.tsx
@@ -1,11 +1,9 @@
+import ImageFormatterForm from "../ImageFormatterForm";
+
 const ImageFormatter = () => {
     return (
-        <div className="flex items-center justify-center">
-            <img
-                src="https://picsum.photos/seed/picsum/800/600"
-                alt="Formatted"
-                className="max-w-full max-h-40 object-cover rounded-lg shadow-md"
-            />
+        <div className="image-formatter">
+            <ImageFormatterForm />
         </div>
     );
 }
--- src/routes/images/formatter/index.tsx
+++ src/routes/images/formatter/index.tsx
@@ -1,11 +1,11 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
+import ImageFormatter from '../../../features/images/components/ImageFormatter'
 
 const ImageFormatterPage = () => {
   return (
     <>
       <h2>Image Formatter</h2>
-      <p>@todo</p>
-      {/* <ImageFormatter /> */}
+      <ImageFormatter />
     </>
   )
 }
