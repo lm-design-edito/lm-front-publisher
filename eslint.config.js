f3f0f73fa5a26a264114b679eb70db6ff1ceaaf2
itsleaht <leaztanda@gmail.com>
2025-06-26 15:20:36 +0200

    packages: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierRecommanded from 'eslint-plugin-prettier/recommended'
+
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierRecommanded],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
