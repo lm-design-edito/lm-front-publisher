e668840725622b6d005e6a626a7a8078ac961f07
itsleaht <leaztanda@gmail.com>
2025-10-07 18:05:11 +0200

    app: add temp waiting screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { useWhoAmI } from './features/authentification/api/use-who-am-i';
 import { router } from './router';
+import { Headline } from '@common-components/headline';
 
 export default function App() {
   const { isAuthenticated, isLoading } = useWhoAmI();
   if (isLoading) {
     return (
-      <div></div>
+      <div className="app-content">
+        <Headline
+          title="LM Publisher"
+          description="The app is loading, you should wait. If it loads undefinetly, please contact us."
+        />
+      </div>
     ); /* You can replace this with a loading spinner or skeleton screen */
   }
   return (
