1b696e7aea9a6ef5e99776d7428ffd4c786efcb2
itsleaht <leaztanda@gmail.com>
2025-10-08 11:17:43 +0200

    chore: update config for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,20 @@ import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react(),
-  ],
-  base: '/lm-front-publisher/',
-  resolve: {
-    alias: {
-      '@api': '/src/api',
-      '@utils': '/src/utils',
-      '@features': '/src/features',
-      '@common-components': '/src/common-components',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+      react(),
+    ],
+    base: command === 'serve' ? '' : '/lm-front-publisher/',
+    resolve: {
+      alias: {
+        '@api': '/src/api',
+        '@utils': '/src/utils',
+        '@features': '/src/features',
+        '@common-components': '/src/common-components',
+      },
     },
-  },
+  }
 });
